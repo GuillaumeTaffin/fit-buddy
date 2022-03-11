b22321a8fe0da0a5f6584c18e48543738f45ee7b
GuillaumeTaffin <73071259+GuillaumeTaffin@users.noreply.github.com>
2022-03-11 17:53:47 -0500

    very simple auth page (#5)

---
+++ src/lib/components/TextField.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+    export let hint = '';
+    export let id = '';
+    export let type = 'text';
+
+    export let text: string;
+
+
+</script>
+
+<input {type}
+       placeholder={hint}
+       id={id}
+       on:change={val => text = val.target.value}
+       class='bg-[color:var(--white)] text-black w-full p-4 outline-[color:var(--primary-dark)] rounded-md'>
