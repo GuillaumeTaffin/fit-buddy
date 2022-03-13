b2589f9d71f7a7ed759b628043d7fa50f138a2a1
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-03-13 09:23:04 -0400

    change a bit the style

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -2,7 +2,7 @@
     import Logo from './Logo.svelte';
 </script>
 
-<div class='shadow-xl p-2 flex items-center space-x-4 bg-[color:var(--primary)]'>
+<div class='p-2 flex items-center space-x-4 bg-[color:var(--primary-dark)]'>
     <Logo class='w-12' />
     <h1 class='text-lg font-medium text-[color:var(--white)] tracking-wide'>FIT BUDDY</h1>
 </div>
