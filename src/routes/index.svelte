7c39f19c37f29e9838edbb07e6e482a957173b2d
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-03-14 16:14:05 -0400

    remove blue highlight that appear on mobile when clicking

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
         <br>
         <div class='grid grid-cols-1 gap-4 max-w-md w-full'>
             <div>
-                <a href='/workouts w-full'>
+                <a href='/workouts'>
                     <Card
                         class='flex flex-col items-center space-y-4 p-4 text-center bg-accent-light/25  ring-2 ring-accent-light shadow-lg shadow-primary-dark active:shadow-none'>
                         <img src='healthy-lifestyle-border.png' alt='workout-logo' class='w-12'>
