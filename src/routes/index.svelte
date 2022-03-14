57b50c18c8b52605bf882b9ca76e8db300a7b992
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-03-14 16:45:41 -0400

    slight color change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
             <div>
                 <a href='/workouts'>
                     <Card
-                        class='flex flex-col items-center space-y-4 p-4 text-center bg-accent-light/25  ring-2 ring-accent-light shadow-lg shadow-primary-dark active:shadow-none'>
+                        class='flex flex-col items-center space-y-4 p-4 text-center bg-accent-light/25 ring-2 ring-accent-light/75 shadow-lg shadow-primary-dark active:shadow-none'>
                         <img src='healthy-lifestyle-border.png' alt='workout-logo' class='w-12'>
                         <p class='text-lg font-medium tracking-wide text-white'>TRAININGS</p>
                     </Card>
@@ -20,7 +20,7 @@
             <div>
                 <a href=''>
                     <Card
-                        class='flex flex-col items-center space-y-4 p-4 text-center bg-accent-light/25 ring-2 ring-accent-light shadow-lg shadow-primary-dark active:shadow-none'>
+                        class='flex flex-col items-center space-y-4 p-4 text-center bg-accent-light/25 ring-2 ring-accent-light/75 shadow-lg shadow-primary-dark active:shadow-none'>
                         <img src='settings.png' alt='workout-logo' class='w-12'>
                         <p class='text-lg font-medium tracking-wide text-white'>SETTINGS</p>
                     </Card>
